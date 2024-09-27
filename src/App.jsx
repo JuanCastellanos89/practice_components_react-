5fa6283877395d476412529959b1030dca7a549e
JuanCastellanos89 <juan.castellanos@est.iudigital.edu.co>
2024-09-26 21:20:17 -0500

    thirdComponent

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,29 @@ import { useState } from 'react'
 import './App.css'
 import { MyFirstComponent } from './components/MyFirstComponent'
 import { SecondComponent } from './components/SecondComponent'
+import { Child } from './components/Child'
+import { ThirdComponent } from './components/ThirdComponent'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState("Laura caro");
 
   return (
     <div className='App'>
       <header clasName="App-header">
-        < SecondComponent />
+        <h1>Fundamentos de React</h1>
+        <hr className='white-hr'/>
+        < ThirdComponent 
+          name="Juan"
+          last_name="Castellanos"
+          age="35"
+        />
+        <hr className='white-hr'/>
+        <Child
+          name={name}
+          setName={setName}
+        />
+        <hr className='white-hr'/>
       </header>
     </div>
   )
---
+++ src/components/Child.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types"
+
+export const Child = (props) => {
+
+    const handleChangeName = () => {
+        const newName = prompt("Ingresa un nuevo nombre: ");
+        if (newName) {
+            props.setName(newName);
+        }
+    };
+
+  return (
+    <div>
+        <h1>Child</h1>
+        <p>Nombre actual: { props.name } </p>
+        <button className="btn btn-primary" onClick={handleChangeName}>
+            cambiar nombre
+        </button>
+    </div>
+  );
+};
+
+Child.PropTypes = {
+    name: PropTypes.string,
+    setName: PropTypes.func,
+};
